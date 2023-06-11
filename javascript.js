bf125e4e9ee0ccd4318bc937a031b4794e5c4520
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-06-11 11:13:38 -0700

    Create add book form and make hidden toggle

--- javascript.js
+++ javascript.js
@@ -1,11 +1,22 @@
+const addBookBtn = document.getElementById("addBookBtn");
+const addBookForm = document.getElementById("addBookForm");
+const library = document.getElementById("library");
+
+
 //Array of book objects
 let myLibrary = [];
 
 
 
 // Constructor function
-function book() {
+function book(newTitle, newAuthor, newStatus) {
+    const newBook = {
+        title: newTitle,
+        author: newAuthor,
+        status: newStatus,
+    }
 
+    myLibrary.push(newBook);
 };
 
 
@@ -14,3 +25,10 @@ function book() {
 function addBook() {
 
 };
+
+
+addBookBtn.addEventListener("click", (e) => {
+    console.log("Clicked book btn");
+    addBookForm.classList.remove("hidden");
+    library.classList.add("fade");
+});
